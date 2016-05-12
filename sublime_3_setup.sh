b4681aa6a519da4516817bb1e7f34311137beb6d
Yuval Gnessin <yuval@autolist.com>
2016-05-12 12:26:04 -0700

    append -custom to filenames in sublime 3 setup

--- sublime_3_setup.sh
+++ sublime_3_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Ruby && cp Ruby.tmLanguage ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Ruby/Ruby.tmLanguage
-mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Rails && cp Ruby\ on\ Rails.tmLanguage ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Rails/Ruby\ on\ Rails.tmLanguage
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Ruby && cp Ruby.tmLanguage ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Ruby/Ruby-custom.tmLanguage
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Rails && cp Ruby\ on\ Rails.tmLanguage ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Rails/Ruby\ on\ Rails-custom.tmLanguage
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default && cp Monokai.tmTheme ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Monokai.tmTheme
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User && cp Default.sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default.sublime-keymap
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Ruby && cp hash-pair-\(-\).sublime-snippet ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Ruby/hash-pair-\(-\).sublime-snippet
